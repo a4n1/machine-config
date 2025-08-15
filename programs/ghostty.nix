11fa9b2b158700ae837e3855c6095793ba503429
Agni <a@a4n1.com>
2025-08-15 07:59:48 +0100

    remove global ghostty shortcuts
    Signed-off-by: Agni <a@a4n1.com>

--- programs/ghostty.nix
+++ programs/ghostty.nix
@@ -7,8 +7,6 @@
       macos-titlebar-style = tabs
       window-padding-x = 12
       window-padding-y = 8
-      keybind = global:ctrl+grave_accent=toggle_quick_terminal
-      keybind = global:cmd+enter=new_window
       ${if builtins.match ".*darwin" system.system == null then "window-decoration = false" else ""}
     '';
   };
