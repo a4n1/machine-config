4388118c3179c429d94b5c494db2328dc91a891b
Agni <a@a4n1.com>
2026-06-29 11:31:24 +0100

    fix deprecated lua config field name
    Signed-off-by: Agni <a@a4n1.com>

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ...}: {
   programs.neovim = {
     enable = true;
-    extraLuaConfig = builtins.readFile ./init.lua;
+    initLua = builtins.readFile ./init.lua;
     plugins = with pkgs.vimPlugins; [
       catppuccin-nvim
       nvim-lspconfig
